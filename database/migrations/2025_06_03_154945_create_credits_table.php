4ad6f6e9a06827b6000735c702eade19f12c3bd5
Matthijs Veldkamp <112868719+MatthijsVeldkamp@users.noreply.github.com>
2025-06-03 15:52:47 +0200

    Table aangemaakt
    id, user_id en credits

---
+++ database/migrations/2025_06_03_154945_create_credits_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('credits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('credits', 8, 2); // Euro's, e.g. 999999.99
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('credits');
+    }
+};
